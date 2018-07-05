716d59f6d1e8ed6a611aa399ecc134b7ff307a0a
Vladimir Svedov <vladimir@pond5.com>
2018-07-05 18:16:34 +0300

    freezing modifications for not "replicated" data

--- tgf_d.sql
+++ tgf_d.sql
@@ -1,6 +1,12 @@
 create or replace function tgf_d() returns trigger as $$
+declare
+  _c int := 0;
 begin
   execute format ('delete from %I where %2$I = ($1).%2$I', 'f_'||TG_RELNAME::regclass, pk(TG_RELNAME)) using OLD;
+  get diagnostics _c:= row_count;
+  if _c < 1 then
+    raise exception '%', 'This data is not replicated yet, thus can''t be deleted';
+  end if;
   return OLD;
 end;
 $$ language plpgsql
--- tgf_u.sql
+++ tgf_u.sql
@@ -5,6 +5,7 @@ declare
   _cols text;
   _vals text;
   _sql text;
+  _c int := 0;
 begin
     _pk := pk(TG_RELNAME);
 
@@ -19,8 +20,11 @@ begin
   raise info '%', NEW;
   execute format('select ($1).%I',_pk) into _val using OLD; --in case update updates the PK value we use OLD, not NEW
   execute format('update %1$I set (%2$s) = (%3$s) from (select ($1).*) row_alias where %1$I.%4$I = %5$s', 'f_'||TG_RELNAME, _cols, _vals, _pk, _val) using NEW;
-  /* at first I formatted %I = %L::%s with atttypid::regtype from pg_attribute, but surely UPDATE SET (LIST)=(NEW) is neater approach...
-  */
+  get diagnostics _c:= row_count;
+  if _c < 1 then
+    raise exception '%', 'This data is not replicated yet, thus can''t be deleted';
+  end if;
+
   return NEW;
 end;
 $$ language plpgsql
